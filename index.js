5df55abd4c5d64b7ef7dd469e644152530261fe5
Aarón <aaronespinozadr@gmail.com>
2025-02-15 15:12:00 -0500

    cambios

--- index.js
+++ index.js
@@ -22,9 +22,11 @@ function calcularPersonaje(){
    $.each($("input[type='radio']:checked"), (i, v)=>{
       puntaje += parseInt($(v).val()) ?? 0;
    });
-
    const personaje = personajes.find(p => puntaje >= p.min && puntaje <= p.max);
+   console.log(puntaje);
+   console.log(personaje);
 
    $("#img-personaje").attr("src", 'image/'+personaje.url);
+   $("#nombre-personaje").text(personaje.nombre);
    $("#descripcion").text(personaje.descripcion);
 }
